1949ff44df70186aac3d434929573c4cb826a325
nitindahiya199 <nitindahiya00000@gmail.com>
2024-04-16 22:57:04 +0530

    dsa practice day 1 - array

---
+++ array/practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Changed the last value to 10 for uniqueness
+    for (int i = 0; i < 10; ++i) {
+        cout << num[i] << " ";
+    }
+    return 0;
+}

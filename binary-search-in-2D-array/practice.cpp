e6bed5dd21b426acccfe781afa4069a2d6f6f90b
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-13 16:27:35 +0530

    day -2 2D array [medium question]

--- binary-search-in-2D-array/practice.cpp
+++ binary-search-in-2D-array/practice.cpp
@@ -16,8 +16,6 @@
 // Explanation:
 //  The row with the maximum number of ones is 0 (0 - indexed).
 
-
-
 // #include <bits/stdc++.h>
 // using namespace std;
 
@@ -64,3 +62,96 @@
 // }
 
 //-------------------------------------------------------------------------------------------------------------------
+
+// You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row are sorted in non-decreasing order. Moreover, the first element of a row is greater than the last element of the previous row (if it exists). You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
+
+// Example :
+// Input Format:
+//  N = 3, M = 4, target = 8,
+// mat[] =
+// 1 2 3 4
+// 5 6 7 8
+// 9 10 11 12
+// Result:
+//  true
+// Explanation:
+//  The ‘target’  = 8 exists in the 'mat' at index (1, 3).
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool find_number(vector<vector<int>> &matrix, int target)
+// {
+//     int n = matrix.size();
+//     int m = matrix[0].size();
+
+//     int low = 0, high = n * m - 1;
+//     while (low <= high)
+//     {
+//         int mid = (low + high) / 2;
+//         int row = mid / m, col = mid % m;
+//         if (matrix[row][col] == target)
+//             return true;
+//         else if (matrix[row][col] < target)
+//             low = mid + 1;
+//         else
+//             high = mid - 1;
+//     }
+
+//     return false;
+// }
+
+// int main()
+// {
+//     vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+//     find_number(matrix, 8) == true ? cout << "True" : cout << "False";
+// }
+
+//---------------------------------------------------------------------------------------------------------------
+
+//  You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row and each column are sorted in non-decreasing order.
+// But, the first element of a row is not necessarily greater than the last element of the previous row (if it exists).
+// You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
+
+// Pre-requisite: Search in a 2D sorted matrix
+
+// Example :
+// Input Format:
+//  N = 5, M = 5, target = 14
+// mat[] = 
+
+// Result:
+//  true
+// Explanation:
+//  Target 14 is present in the cell (3, 2)(0-based indexing) of the matrix. So, the answer is true.
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchElement(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int row = 0, col = m - 1;
+
+    //traverse the matrix from (0, m-1):
+    while (row < n && col >= 0) {
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) row++;
+        else col--; 
+    }
+    return false;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+

95830d54add948bd82c7630e8778577af10d0fa9
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-14 16:49:56 +0530

    find median in 2D array

--- binary-search-in-2D-array/practice.cpp
+++ binary-search-in-2D-array/practice.cpp
@@ -63,7 +63,7 @@
 
 //-------------------------------------------------------------------------------------------------------------------
 
-// You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row are sorted in non-decreasing order. Moreover, the first element of a row is greater than the last element of the previous row (if it exists). You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
+// Q : You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row are sorted in non-decreasing order. Moreover, the first element of a row is greater than the last element of the previous row (if it exists). You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
 
 // Example :
 // Input Format:
@@ -109,7 +109,7 @@
 
 //---------------------------------------------------------------------------------------------------------------
 
-//  You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row and each column are sorted in non-decreasing order.
+//  Q : You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively. The elements of each row and each column are sorted in non-decreasing order.
 // But, the first element of a row is not necessarily greater than the last element of the previous row (if it exists).
 // You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
 
@@ -118,40 +118,105 @@
 // Example :
 // Input Format:
 //  N = 5, M = 5, target = 14
-// mat[] = 
+// mat[] =
 
 // Result:
 //  true
 // Explanation:
 //  Target 14 is present in the cell (3, 2)(0-based indexing) of the matrix. So, the answer is true.
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int find_number(vector<vector<int>> &arr, int target)
+// {
+//     int n = arr.size();
+//     int m = arr[0].size();
+//     int row = 0, col = m - 1;
+
+//     while (row < n && col >= 0)
+//     {
+//         if (arr[row][col] == target)
+//             return true;
+//         else if (arr[row][col] < target)
+//             row++;
+//         else
+//             col--;
+//     }
+//     return false;
+// }
+
+// int main()
+// {
+//     vector<vector<int>> arr = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+
+//     find_number(arr, 8) == true ? cout << "true" : cout << "False";
+// }
 
 
+//------------------------------------------------------------------------------------------------------------------
+
+// Q :Given a row-wise sorted matrix of size r*c, where r is no. of rows and c is no. of columns, find the median in the given matrix.
+
+// Assume - r*c is odd
+
+// Example :
+// Input: 
+// r = 3 , c = 3
+// 1 4 9 
+// 2 5 6
+// 3 8 7
+// Output: Median = 5
+// Explanation: If we find the linear sorted array, the array becomes 1 2 3 4 5 6 7 8 9
+// So, median = 5
 
 
 #include <bits/stdc++.h>
 using namespace std;
-
-bool searchElement(vector<vector<int>>& matrix, int target) {
-    int n = matrix.size();
-    int m = matrix[0].size();
-    int row = 0, col = m - 1;
-
-    //traverse the matrix from (0, m-1):
-    while (row < n && col >= 0) {
-        if (matrix[row][col] == target) return true;
-        else if (matrix[row][col] < target) row++;
-        else col--; 
+int countSmallerThanMid(vector<int> &row, int mid)
+{
+  int l = 0, h = row.size() - 1;
+  while (l <= h)
+  {
+    int md = (l + h) >> 1;
+    if (row[md] <= mid)
+    {
+      l = md + 1;
     }
-    return false;
+    else
+    {
+      h = md - 1;
+    }
+  }
+  return l;
 }
-
+int findMedian(vector<vector<int>> &A)
+{
+  int low = 1;
+  int high = 1e9;
+  int n = A.size();
+  int m = A[0].size();
+  while (low <= high)
+  {
+    int mid = (low + high) >> 1;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+      cnt += countSmallerThanMid(A[i], mid);
+    }
+    if (cnt <= (n * m) / 2)
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+  return low;
+}
 int main()
 {
-    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
-        {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
-        {18, 21, 23, 26, 30}
-    };
-    searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+  int row = 3, col = 3;
+  vector<vector<int>> arr = {{1, 3, 8},
+                             {2, 3, 4},
+                             {1, 2, 5}};
+  cout <<"The median of the row-wise sorted matrix is: "<<findMedian(arr)<<endl;
+  return 0;
 }
-

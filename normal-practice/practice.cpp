6fb494b15f388797fece1e9bcaefda926cd15250
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-18 16:50:37 +0530

    normal practice day 2

--- normal-practice/practice.cpp
+++ normal-practice/practice.cpp
@@ -125,32 +125,111 @@
 // Explanation:
 //  Total number of unique elements are 3, i.e[1,2,3] and Therefore return 3 after assigning [1,2,3] in the beginning of the array.
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int remove_duplicate(vector<int>& arr) {
+//     int i = 0;
+//     for (int j = 1; j < arr.size(); j++) {
+//         if (arr[i] != arr[j]) {
+//             i++;
+//             arr[i] = arr[j];
+//         }
+//     }
+//     return i + 1;
+// }
+
+// int main() {
+//     vector<int> arr = {1, 1, 2, 2, 2, 3, 3};
+//     int output = remove_duplicate(arr);
+
+//     for (int i = 0; i < output; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+//---------------------------------------------------------------------------------------------------------------
+
+// Q: Given an array of N integers, left rotate the array by one place.
+
+// Example :
+// Input:
+//  N = 5, array[] = {1,2,3,4,5}
+// Output:
+//  2,3,4,5,1
+// Explanation:
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void rotate_one(vector<int> arr, int n)
+// {
+//     int cage[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cage[i - 1] = arr[i];
+//     }
+
+//     cage[n - 1] = arr[0];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << cage[i] <<  " ";
+//     }
+
+//     cout << endl;
+
+// }
+
+// int main()
+// {
+//     vector<int> arr = {1, 2, 3, 4, 5};
+//     rotate_one(arr, 5);
+// }
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// Q: Given an array of integers, rotating array of elements by k elements either left or right.
+
+// Example :
+// Input: N = 7, array[] = {1,2,3,4,5,6,7} , k=2 , right
+// Output: 6 7 1 2 3 4 5
+// Explanation: array is rotated to right by 2 position .
+
+#include <iostream>
 using namespace std;
-
-int remove_deplicate(vector<int> arr, int n)
+void Rotatetoright(int arr[], int n, int k)
 {
-    int i = 0;
-
-    for (int j = 0; j < 7; j++)
-
-        if (arr[i] != arr[j])
-        {
-            {
-                i++;
-                arr[i] = arr[j];
-            }
-        }
-    return i + 1;
+    if (n == 0)
+        return;
+    k = k % n;
+    if (k > n)
+        return;
+    int temp[k];
+    for (int i = n - k; i < n; i++)
+    {
+        temp[i - n + k] = arr[i];
+    }
+    for (int i = n - k - 1; i >= 0; i--)
+    {
+        arr[i + k] = arr[i];
+    }
+    for (int i = 0; i < k; i++)
+    {
+        arr[i] = temp[i];
+    }
 }
-
 int main()
 {
-    vector<int> arr = {1, 1, 2, 2, 2, 3, 3};
-    int output = remove_deplicate(arr, 7);
-
-    for (int i = 0; i < output; i++)
+    int n = 7;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int k = 2;
+    Rotatetoright(arr, n, k);
+    cout << "After Rotating the elements to right " << endl;
+    for (int i = 0; i < n; i++)
     {
-        cout << arr[i]
+        cout << arr[i] << " ";
     }
+    return 0;
 }

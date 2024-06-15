0b800f048533d2e836a51a1adb52e46c5c78873c
nitindahiya199 <nitindahiya00000@gmail.com>
2024-06-15 15:52:08 +0530

    say yes to dsa

--- bits-manipulation/practice.cpp
+++ bits-manipulation/practice.cpp
@@ -122,7 +122,7 @@
 //             } else {
 //                 n[i] = '1';
 
-//             }           
+//             }
 //         }
 //     }
 //     return n;
@@ -130,10 +130,169 @@
 
 // int main(){
 //     string input = "111101";
-//     string first_ans = first_complement(input); 
+//     string first_ans = first_complement(input);
 //     string second_ans = second_complement(first_ans);
 
-//     cout << "without complement: " << input << endl; 
-//     cout << "first complement: " << first_ans << endl; 
-//     cout << "second complement: " << second_ans; 
+//     cout << "without complement: " << input << endl;
+//     cout << "first complement: " << first_ans << endl;
+//     cout << "second complement: " << second_ans;
 // }
+
+// -------------------------------------------------------------------------------------------------
+
+// Swap 2 Numbers
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int a = 6;  // initial value of a
+//     int b = 7;  // initial value of b
+
+//     cout << "Before swap, a is: " << a << endl;
+//     cout << "Before swap, b is: " << b << endl;
+
+//     // Step 1: a = a ^ b
+//     a = a ^ b;
+//     cout << "After step 1, a is: " << a << " (a ^ b)" << endl;
+//     cout << "After step 1, b is: " << b << " (unchanged)" << endl;
+
+//     // Step 2: b = a ^ b
+//     b = a ^ b;
+//     cout << "After step 2, a is: " << a << " (unchanged)" << endl;
+//     cout << "After step 2, b is: " << b << " (a ^ b ^ b = original a)" << endl;
+
+//     // Step 3: a = a ^ b
+//     a = a ^ b;
+//     cout << "After step 3, a is: " << a << " (a ^ b ^ a = original b)" << endl;
+//     cout << "After step 3, b is: " << b << " (unchanged)" << endl;
+
+//     cout << "After swap, a is: " << a << endl;
+//     cout << "After swap, b is: " << b << endl;
+
+//     return 0;
+// }
+
+// -----------------------------------------------------------------------------------------------
+
+// check if the ith bits is set or not ?
+
+// N = 13  i = 2  => [1 1 0 1] i = 1; because 0 = 1, 1 = 0, and 2 = 1;
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// string convert_it(int n)
+// {
+//     string binary_str = "";
+//     while (n > 0)
+//     {
+//         if (n % 2 == 1)
+//         {
+//             binary_str += '1';
+//         }
+//         else
+//         {
+//             binary_str += '0';
+//         }
+//         n /= 2;
+//     }
+//     reverse(binary_str.begin(), binary_str.end());
+//     return binary_str;
+// }
+
+// bool find_set(string binary_str, int k)
+// {
+//     if (k >= 0 && k < binary_str.size())
+//     {
+//         return (binary_str[k] == '1');
+//     }
+//     return false;
+// }
+
+// int main()
+// {
+//     int input = 13;
+//     string output = convert_it(input);
+//     cout << "binary of 13 is: " << output << endl;
+//     bool ans = find_set(output, 0);
+//     if (ans)
+//     {
+//         cout << "yes it is a set" << endl;
+//     }
+//     else
+//     {
+//         cout << "No it isn't a set" << endl;
+//     }
+// }
+
+// -------------------------------------------------------------------------------------------------
+
+// change integer to binary then
+// Set the ith bit 1, if it already 1 then don't do anything
+// then back to index
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// string convert_it(int n)
+// {
+
+//     if (n == 0) {
+//         return "0";  // Special case for zero
+//     }
+
+
+//     string binary_str = "";
+//     while (n > 0)
+//     {
+//         if (n % 2 == 1)
+//         {
+//             binary_str += '1';
+//         }
+//         else
+//         {
+//             binary_str += '0';
+//         }
+
+//         n /= 2;
+//     }
+//     reverse(binary_str.begin(), binary_str.end());
+//     return binary_str;
+// }
+
+// string change_it(string xx, int k)
+// {
+//     if (k > 0 && k <= xx.size())
+//     {
+//        if(xx[k] == '1' || xx[k] == '0'){
+//         xx[k] = '1';
+//        }
+//     }
+//     return xx;
+// }
+
+// int convert_it_back(string binary_str)
+// {
+//     int digit = 0;
+//     for (int i = 0; i < binary_str.size(); i++)
+//     {
+//         if(binary_str[i] == '1'){
+//             digit+= pow(2,binary_str.size()-1-i);
+//          }
+//     }
+//     return digit;
+// }
+
+// int main()
+// {
+//     int input = 13;
+//     string output = convert_it(input);
+//     cout << "binary version: " << output << endl;
+
+//     string ans = change_it(output, 2);
+//     cout << "After change: " << ans << endl;
+
+//     int ans2 = convert_it_back(ans);
+//     cout << "After change back to number: " << ans2 << endl;
+// }
+

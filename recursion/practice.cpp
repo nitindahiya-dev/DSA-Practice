f857980dd86c6f66855da55402682c486b749af8
nitindahiya199 <nitindahiya00000@gmail.com>
2024-06-24 12:41:42 +0530

    more dsa

--- recursion/practice.cpp
+++ recursion/practice.cpp
@@ -208,3 +208,36 @@
 
 //     return 0;
 // }
+
+// -------------------------------------------------------------------------------------------------
+
+// Print fibonacci numbers..!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n)
+{
+    if (n <= 1)
+        return n;
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    fibo(n);
+    if (n <= 0)
+    {
+        cout << "Please enter a positive number." << endl;
+    }
+
+    cout << "Your Fibonacci series is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << fibo(i) << " ";
+    }
+    cout << endl;
+}

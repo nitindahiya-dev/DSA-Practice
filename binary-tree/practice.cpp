8de60a8eb453e7353c73ed7d53edb1fcf21c70c2
nitindahiya-dev <nitindahiya00000@gmail.com>
2024-10-11 15:47:43 +0530

    tree

--- binary-tree/practice.cpp
+++ binary-tree/practice.cpp
@@ -1858,3 +1858,39 @@
 // }
 
 // ------------------------------------------------------------------------------------------------------   
+
+// inorder binary tree
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int val;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int v): val(v), left(nullptr), right(nullptr){};
+};
+
+
+void inorderTraversal(struct Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+    inorderTraversal(node->left);   // Visit left subtree
+    cout << node->val << " ";     // Print node data
+    inorderTraversal(node->right);  // Visit right subtree
+}
+
+int main() {
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->right = new Node(5);
+
+    cout << "In-order traversal of the binary tree: ";
+    inorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
